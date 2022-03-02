08615b7c013f7a072d7cca1b4e2f522fbfed6296
ttsuki <tu3+github@tu3.jp>
2022-03-03 08:33:48 +0900

    arkana.lib: add sha2-avx2 and impl-dispacher

--- arkana/sha2/sha2.cpp
+++ arkana/sha2/sha2.cpp
@@ -7,29 +7,74 @@
 /// https://opensource.org/licenses/MIT
 
 #include "../sha2.h"
+
+#include "../bits/cpuid.h"
+
+namespace arkana::sha2
+{
+    std::unique_ptr<sha224_context_t> create_sha224_context()
+    {
+        return cpuid::cpu_supports::AVX2
+                   ? create_sha224_context_avx2()
+                   : create_sha224_context_ref();
+    }
+
+    std::unique_ptr<sha256_context_t> create_sha256_context()
+    {
+        return cpuid::cpu_supports::AVX2
+                   ? create_sha256_context_avx2()
+                   : create_sha256_context_ref();
+    }
+
+    std::unique_ptr<sha384_context_t> create_sha384_context()
+    {
+        return cpuid::cpu_supports::AVX2
+                   ? create_sha384_context_avx2()
+                   : create_sha384_context_ref();
+    }
+
+    std::unique_ptr<sha512_context_t> create_sha512_context()
+    {
+        return cpuid::cpu_supports::AVX2
+                   ? create_sha512_context_avx2()
+                   : create_sha512_context_ref();
+    }
+}
+
 #include "./sha2.h"
 
 namespace arkana::sha2
 {
-    namespace // internal linkage
+    template <class sha2_state_t>
+    static inline auto make_sha2_context_ref(sha2_state_t state)
     {
-        template <
-            auto create_sha2_state,
-            class sha2_state_t = decltype(create_sha2_state()),
-            size_t digest_bits = sha2_state_t::digest_bits>
         class sha2_context_impl_t
-            : public virtual sha2_context_t<sha2_digest_t<digest_bits>>
+            : public virtual sha2_context_t<sha2_digest_t<sha2_state_t::digest_bits>>
         {
         public:
-            sha2_state_t state = create_sha2_state();
+            sha2_state_t state;
 
-            void process_bytes(const void* data, size_t len) noexcept override { arkana::sha2::process_bytes(state, data, len); }
-            sha2_digest_t<digest_bits> finalize() noexcept override { return arkana::sha2::finalize_and_get_digest(state); }
+            explicit sha2_context_impl_t(sha2_state_t state)
+                : state(std::move(state))
+            {
+            }
+
+            void process_bytes(const void* data, size_t len) noexcept override
+            {
+                ref::process_bytes(state, data, len);
+            }
+
+            sha2_digest_t<sha2_state_t::digest_bits> finalize() noexcept override
+            {
+                return ref::finalize_and_get_digest(state);
+            }
         };
+
+        return std::make_unique<sha2_context_impl_t>(std::move(state));
     }
 
-    std::unique_ptr<sha224_context_t> create_sha224_context() { return std::make_unique<sha2_context_impl_t<create_sha224_state>>(); }
-    std::unique_ptr<sha256_context_t> create_sha256_context() { return std::make_unique<sha2_context_impl_t<create_sha256_state>>(); }
-    std::unique_ptr<sha384_context_t> create_sha384_context() { return std::make_unique<sha2_context_impl_t<create_sha384_state>>(); }
-    std::unique_ptr<sha512_context_t> create_sha512_context() { return std::make_unique<sha2_context_impl_t<create_sha512_state>>(); }
+    std::unique_ptr<sha224_context_t> create_sha224_context_ref() { return make_sha2_context_ref(create_sha224_state()); }
+    std::unique_ptr<sha256_context_t> create_sha256_context_ref() { return make_sha2_context_ref(create_sha256_state()); }
+    std::unique_ptr<sha384_context_t> create_sha384_context_ref() { return make_sha2_context_ref(create_sha384_state()); }
+    std::unique_ptr<sha512_context_t> create_sha512_context_ref() { return make_sha2_context_ref(create_sha512_state()); }
 }
