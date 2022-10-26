0810cb89d7f8c04efd292d286cae33d2bded5e86
ttsuki <ttsuki@ttsuki.dev>
2022-10-26 18:30:57 +0900

    add ark::tagged_memory_buffer

--- arkana/ark/types.h
+++ arkana/ark/types.h
@@ -25,4 +25,12 @@ namespace arkana
 
     template <class T, class... Ts> using is_any_of = std::disjunction<std::is_same<T, Ts>...>;
     template <class T, class... Ts> static constexpr bool is_any_of_v = is_any_of<T, Ts...>::value;
+
+    /// tagged_memory_buffer
+    template <class tag, size_t size, class alignment = void*>
+    struct alignas(alignment) tagged_memory_buffer final
+    {
+        using tag_t = tag;
+        byte_array<size> buffer;
+    };
 }
